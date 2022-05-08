2dc01a66c86e1d9763ed3ed5b4015abe5b664b04
Ankit <ankitthakur1998@gmail.com>
2022-05-08 15:48:22 +0530

    inject properties for: coach.name & team.name

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,11 +2,20 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	
+	// inject properties for: coach.name & team.name
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${coach.name}")
+	private String teamName;
+	
 
 	//expose "/" that return "Hello World"
 	
